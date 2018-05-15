29f3557ba027345de8e608cd80f28a7246e09008
Pedro Freitas <pvpfreitas@gmail.com>
2018-05-15 14:32:48 -0300

    recreating folder static

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -131,6 +131,11 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
